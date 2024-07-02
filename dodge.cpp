f9cba5c7f2e2e20ccc07d0a92c05b16e179b4312
Grad Laurentiu <gradlaur@yahoo.com>
2024-07-02 22:50:42 +0300

    gavel of haima

--- dodge.cpp
+++ dodge.cpp
@@ -1,30 +1,82 @@
 #include "functions.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <string>
+
+car my_car = create_car();
+std::vector<obstacle> my_obstacles;    
+
+void * obstacle_generation(void * arg) {
+    sleep(2);
+
+    while(true) {
+        point p1, p2;
+        p1.x = 700;
+        p2.x = 800;
+        p1.y = rand() % 500 + 1;
+        p2.y = p1.y + 100;
+        obstacle o = create_obstacle(p1, p2, GREEN);
+        my_obstacles.push_back(o);
+        sleep(2);
+    }
+    return NULL;
+}
+
+void stop_game(pthread_t thread) {
+    pthread_cancel(thread);
+    ClearBackground(BACKGROUND_COLOR);
+    DrawText("GAME OVER!", 320, 250, 20, WHITE);
+    EndDrawing();
+    sleep(1);
+}
+
+void int_to_char(int x, char * s) {
+    if(x == 0) {
+        s[0] = '0';
+        s[1] = '\0';
+        return;
+    }
+    int i = 0;
+    while(x) {
+        s[i++] = (char)(x % 10 + '0');
+        x /= 10;
+    }
+    s[i] = '\0';
+}
 
 int main() {
     InitWindow(window_width, window_height, "DODGE OBSTACLES");
 
-    // point circle_center = {200, 200};
-    // float circle_radius = 50.0f;  
+    SetTargetFPS(80);
 
-    SetTargetFPS(120);
-    car my_car = create_car();
-    obstacle my_obstacle = create_obstacle();
+    pthread_t obstacle_thread;
+    if(pthread_create(&obstacle_thread, NULL, obstacle_generation, NULL) < 0) {
+        printf("Error!\n");
+        exit(-1);
+    }
+
+    score_int = 0;
+    char score_char[20] = "SCORE: "; 
     bool stop = false;
-    while(!WindowShouldClose() && !stop) {
+    while(!stop) {
         BeginDrawing();
         ClearBackground(BACKGROUND_COLOR);
         
-        print_obstacle(my_obstacle);
+        int_to_char(score_int, score_char + 7);
+        DrawText(score_char, 10, 10, 20, WHITE);
 
+        print_obstacles(my_obstacles);
         print_car(my_car);
-        handle_car(my_car);
 
-        stop = handle_collision(my_car, my_obstacle);
+        stop = handle_collision(my_car, my_obstacles) | WindowShouldClose();
 
-        EndDrawing();
-    }
-    if(stop){
-        ;
+        if(stop)
+            stop_game(obstacle_thread);
+        else {
+            handle_obstacles(my_obstacles);
+            handle_car(my_car);
+            EndDrawing();
+        }
     }
 
     return 0;
